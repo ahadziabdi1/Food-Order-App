b0bafe765d18700cc4c96740e386abbf3a1cc5db
ahadziabdi1 <ahadziabdi1@etf.unsa.ba>
2025-12-01 13:21:21 +0100

    Refactor title in HTML and Header component; add currency formatting utility

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
         <header id="main-header">
             <div id="title">
                 <img src={logoImg} alt='A restaurant' />
-                <h1>ReactFood</h1>
+                <h1>Food Order</h1>
             </div>
             <nav>
                 <button>Cart (0)</button>
